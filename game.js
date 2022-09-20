2ba667f164401fcf9ce9425b01942760d413d192
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-20 14:04:59 +0200

    guessing part completed

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
+let score = 10;
+const body = document.querySelector("body");
+let randomSayi;
+document.querySelector(".again").onclick = () => {
+  body.style.backgroundColor = "gray";
+  randomSayi = Math.floor(Math.random() * 21);
+  console.log(randomSayi);
+  score = 10;
+  document.querySelector(".score").textContent = score;
+};
 
+document.querySelector(".check").onclick = () => {
+  let guess = document.querySelector(".guess");
+
+  if (Number(guess.value) < randomSayi) {
+    document.querySelector(".msg").textContent = "Sayiyi Yükseltin";
+    body.style.backgroundColor = "red";
+    score--;
+  } else if (Number(guess.value) > randomSayi) {
+    document.querySelector(".msg").textContent = "Sayiyi Azaltin";
+    body.style.backgroundColor = "red";
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else {
+    body.style.backgroundColor = "green";
+    document.querySelector(".msg").textContent = "Tebrikler Bildiniz...";
+  }
+};
