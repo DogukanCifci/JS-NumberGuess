12404fa35a0e20dc25ffa8c0828e8ecb4a6ba861
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-09-20 22:24:09 +0200

    guess number something edited

--- game.js
+++ game.js
@@ -1,24 +1,14 @@
 let score = 10;
 const body = document.querySelector("body");
 let topScore = 0;
-let randomSayi;
+
 const guess = document.querySelector(".guess");
 let status1 = false;
-
-document.querySelector(".again").onclick = () => {
-  score = 10;
-  console.log("bastiktan sonra score", score);
-  body.style.backgroundColor = "gray";
-  randomSayi = Math.floor(Math.random() * 21);
-  console.log(randomSayi);
-
-  document.querySelector(".score").textContent = score;
-  document.querySelector(".msg").textContent = "Starting..";
-  document.querySelector(".number").textContent = "?";
-};
-
+let randomSayi = Math.floor(Math.random() * 21);
 document.querySelector(".check").onclick = () => {
-  if (Number(guess.value) < randomSayi && score > 0) {
+  if (guess.value < 0 || guess.value > 20) {
+    alert("Lütfen 0-20 arasinda bir sayi giriniz!");
+  } else if (Number(guess.value) < randomSayi && score > 0) {
     document.querySelector(".msg").textContent = "Sayiyi Yükseltin 🔼";
     body.style.backgroundColor = "red";
     guess.value = "";
@@ -30,7 +20,7 @@ document.querySelector(".check").onclick = () => {
     guess.value = "";
     score--;
     document.querySelector(".score").textContent = score;
-  } else if (Number(guess.value) == randomSayi && score > 0) {
+  } else if (Number(guess.value) == randomSayi && score != 0) {
     body.style.backgroundColor = "green";
     const alkis = document.querySelector("#alkis");
     alkis.play();
@@ -39,13 +29,27 @@ document.querySelector(".check").onclick = () => {
     document.querySelector(".number").textContent = randomSayi;
 
     if (score > topScore) {
-      document.querySelector(".top-score").textContent = score;
       topScore = score;
+      document.querySelector(".top-score").textContent = topScore;
     }
+    score = 0;
   } else if (score == 0) {
     document.querySelector(".msg").textContent = "GAME OVER";
     document.querySelector(".wrong").play();
   }
+};
+document.querySelector(".again").onclick = () => {
+  document.querySelector("#alkis").pause();
+  score = 10;
+  console.log("bastiktan sonra score", score);
+  body.style.backgroundColor = "gray";
+  randomSayi = Math.floor(Math.random() * 21);
+  console.log(randomSayi);
+
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".msg").textContent = "Starting..";
+  document.querySelector(".number").textContent = "?";
+  guess.value = "";
 };
 
 guess.onkeydown = (tus) => {
